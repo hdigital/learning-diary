28ba3d02e8093b770ea16b09790f8c44a8407f9e
Holger Döring <doering@uni-bremen.de>
2024-06-13 12:30:56 +0200

    Create and sync venv in render all script

--- snippets/render-all-posts.sh
+++ snippets/render-all-posts.sh
@@ -1,3 +1,8 @@
+# create and sync local Python environment
+uv venv --allow-existing
+uv pip sync requirements.txt
+source .venv/bin/activate
+
 # install post packages
 Rscript -e "pak::lockfile_install(update = FALSE)"
 uv pip install -r snippets/requirements-posts.txt
