fc19486c0508d68c4d6194f4316e9d3c2c75ee40
Holger Döring <doering@uni-bremen.de>
2024-07-16 16:02:11 +0200

    Update script to render all posts
    - use venv/pip instead of uv
    - update posts to render
    - git ignore Python history file
    - check successful local run

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ RUN apt-get -y update && apt-get install -y --no-install-recommends \
   python3 \
   python3-dev \
   python3-pip \
+  python3.10-venv \
   python-is-python3
 
 # Install Quarto
--- snippets/render-all-posts.sh
+++ snippets/render-all-posts.sh
@@ -1,14 +1,19 @@
+#!/bin/bash
+
+# remove existing Python environment
+rm -rf .venv
+
 # create and sync local Python environment
-uv venv --allow-existing
-uv pip sync requirements.txt
+python -m venv .venv
 source .venv/bin/activate
+python -m pip install -r requirements.txt
 
 # install post packages
 Rscript -e "pak::lockfile_install(update = FALSE)"
-uv pip install -r snippets/requirements-posts.txt
+python -m pip install -r snippets/requirements-posts.txt
 
 # remove freeze folder for full rebuild
-rm -r _freeze
+rm -rf _freeze
 
 # render all posts
 quarto render
